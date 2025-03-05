828f8e5fefc5d3725e43793d98b3bf21fe6a98c1
Lucas Magnago de Oliveira <lucas.magnago.02@gmail.com>
2025-03-04 23:35:42 -0300

    Estrutura visual - Criando carrossel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,26 @@
-import { HomeContainer, Product } from "../styles/pages/home";
-
 import Image from 'next/image';
+
+import { useKeenSlider } from 'keen-slider/react';
+
+import { HomeContainer, Product } from "../styles/pages/home";
 
 import shirt1 from '../assets/shirts/1.png';
 import shirt2 from '../assets/shirts/2.png';
 import shirt3 from '../assets/shirts/3.png';
 
+import 'keen-slider/keen-slider.min.css';
+
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  });
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className='keen-slider'>
+      <Product className='keen-slider__slide'>
         <Image src={shirt1.src} width={520} height={480} alt={""} />
 
         <footer>
@@ -19,7 +30,7 @@ export default function Home() {
           </span>
         </footer>
       </Product>
-      <Product>
+      <Product className='keen-slider__slide'>
         <Image src={shirt2} width={520} height={480} alt={""} />
 
         <footer>
@@ -29,7 +40,27 @@ export default function Home() {
           </span>
         </footer>
       </Product>
-      <Product>
+      <Product className='keen-slider__slide'>
+        <Image src={shirt3} width={520} height={480} alt={""} />
+
+        <footer>
+          <strong>Camiseta 3</strong>
+          <span>
+            R$ 129,90
+          </span>
+        </footer>
+      </Product>
+      <Product className='keen-slider__slide'>
+        <Image src={shirt3} width={520} height={480} alt={""} />
+
+        <footer>
+          <strong>Camiseta 3</strong>
+          <span>
+            R$ 129,90
+          </span>
+        </footer>
+      </Product>
+      <Product className='keen-slider__slide'>
         <Image src={shirt3} width={520} height={480} alt={""} />
 
         <footer>
